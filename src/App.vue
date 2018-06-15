9ca996c03b34685ce128c29f9004b7054b3f4bc7
right2drive <bradpf@outlook.com>
2018-06-14 20:53:20 -0600

    Setting up basic split screen design

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $backgroundColor;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@import url('https://fonts.googleapis.com/css?family=Bungee');
+
+body {
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+  font-family: Georgia, serif;
+
+  #app {
+    height: 100%;
+    width: 100%;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="panel left">
+      <p>Stream</p>
+    </div>
+    <div class="panel right">
+      <p>Share</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-}
 </script>
+
+<style lang="scss" scoped>
+
+.home {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+
+  .panel {
+    flex: 1 0 auto;
+    width: 0;
+    background-color: $lightHome;
+    transition: background-color $transitionTime ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      background-color: $darkHome;
+
+      > p {
+        color: $white;
+      }
+    }
+
+    > p {
+      color: $black;
+      font-size: 50px;
+      font-family: 'Bungee', cursive;
+      transition: color $transitionTime ease;
+    }
+  }
+
+}
+</style>
