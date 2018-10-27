00c2f1b60301245909a590c4f5770fc5b2a3d35f
Brad <bradpf@outlook.com>
2018-10-27 16:35:56 -0600

    Adding manifest (#45)
    * Adding manifest
    
    * Fixing theme
    
    * Making standalone

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,7 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const SriPlugin = require('webpack-subresource-integrity')
 const CopyPlugin = require('copy-webpack-plugin')
+const ManifestPlugin = require('webpack-pwa-manifest')
 
 const createConfig = require('./base.config')
 const { root } = require('./util')
@@ -43,7 +44,22 @@ function prodConfig () {
           from: root('public', '.well-known'),
           to: root('dist', '.well-known')
         }
-      ])
+      ]),
+      new ManifestPlugin({
+        fingerprints: false,
+        name: 'WeSync Video Client',
+        short_name: 'WeSync',
+        description: 'Video streaming application for local files with synced controls',
+        background_color: '#616161',
+        theme_color: '#212121',
+        display: 'standalone',
+        icons: [
+          {
+            src: root('assets', 'favicon.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+          }
+        ]
+      })
     ]
   })
 }
